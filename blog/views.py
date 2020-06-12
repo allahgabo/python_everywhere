f3501c52d8cda0512a51d8fb544d78edb775fb07
suliman allahgabo <suliman_allahgabo@yahoo.com>
2020-06-12 23:08:26 +0530

    'everywhere'

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,8 @@ from django.views.generic import (
 )
 from .models import Post
 
+def index(request):
+    return render(request, 'blog/index.html', {'title': 'index'})
 
 def home(request):
     context = {
